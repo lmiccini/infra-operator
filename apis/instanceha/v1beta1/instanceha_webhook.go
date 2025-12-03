250f8c0f006d557aa2836809a950517cccf9ae7a
Luca Miccini <lmiccini@redhat.com>
2025-12-03 08:11:46 +0100

    test dan patch

--- apis/instanceha/v1beta1/instanceha_webhook.go
+++ apis/instanceha/v1beta1/instanceha_webhook.go
@@ -21,7 +21,6 @@ package v1beta1
 
 import (
 	"k8s.io/apimachinery/pkg/runtime"
-	ctrl "sigs.k8s.io/controller-runtime"
 	logf "sigs.k8s.io/controller-runtime/pkg/log"
 	"sigs.k8s.io/controller-runtime/pkg/webhook"
 	"sigs.k8s.io/controller-runtime/pkg/webhook/admission"
@@ -46,15 +45,6 @@ func SetupInstanceHaDefaults(defaults InstanceHaDefaults) {
 	instancehalog.Info("InstanceHa defaults initialized", "defaults", defaults)
 }
 
-// SetupWebhookWithManager sets up the webhook with the Manager
-func (r *InstanceHa) SetupWebhookWithManager(mgr ctrl.Manager) error {
-	return ctrl.NewWebhookManagedBy(mgr).
-		For(r).
-		Complete()
-}
-
-//+kubebuilder:webhook:path=/mutate-client-openstack-org-v1beta1-instanceha,mutating=true,failurePolicy=fail,sideEffects=None,groups=client.openstack.org,resources=instancehas,verbs=create;update,versions=v1beta1,name=minstanceha.kb.io,admissionReviewVersions=v1
-
 var _ webhook.Defaulter = &InstanceHa{}
 
 // Default implements webhook.Defaulter so a webhook will be registered for the type
@@ -70,9 +60,6 @@ func (spec *InstanceHaSpec) Default() {
 		spec.ContainerImage = instanceHaDefaults.ContainerImageURL
 	}
 }
-
-// TODO(user): change verbs to "verbs=create;update;delete" if you want to enable deletion validation.
-//+kubebuilder:webhook:path=/validate-client-openstack-org-v1beta1-instanceha,mutating=false,failurePolicy=fail,sideEffects=None,groups=client.openstack.org,resources=instancehas,verbs=create;update,versions=v1beta1,name=vinstanceha.kb.io,admissionReviewVersions=v1
 
 var _ webhook.Validator = &InstanceHa{}
 
