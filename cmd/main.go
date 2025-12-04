36779491082e4af949b71fd4a04d929e3a975d3a
Luca Miccini <lmiccini@redhat.com>
2025-12-04 06:41:22 +0100

    svc

--- cmd/main.go
+++ cmd/main.go
@@ -307,6 +307,14 @@ func main() {
 		setupLog.Error(err, "unable to create controller", "controller", "DNSData")
 		os.Exit(1)
 	}
+	if err := (&networkcontroller.ServiceReconciler{
+		Client:  mgr.GetClient(),
+		Kclient: kclient,
+		Scheme:  mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "Service")
+		os.Exit(1)
+	}
 	if err := (&networkcontroller.IPSetReconciler{
 		Client:  mgr.GetClient(),
 		Scheme:  mgr.GetScheme(),
